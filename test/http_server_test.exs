73877b44025838737e11bc2049510c33311fb13e
lina ngo <linango@MacBookPro.modem>
2025-03-27 15:37:11 +1100

    test modified to use Task. template to render added for sensors

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -30,6 +30,10 @@ defmodule HttpServerTest do
     #   assert remove_whitespace(response) == remove_whitespace(expected_response)
     # end
 
+    # defp remove_whitespace(text) do
+    #     String.replace(text, ~r{\s}, "")
+    # end
+
     test "accepts a request on a socket and sends back a response" do
         spawn(HttpServer, :start, [4000])
 
@@ -39,33 +43,55 @@ defmodule HttpServerTest do
         assert response.body == "Bears, Lions, Tigers"
     end
 
-    test "spawn 5 requests concurrently" do
+    # test "spawn 5 requests concurrently" do
+    #     spawn(HttpServer, :start, [4000])
+
+    #     parent = self()
+
+    #     max_req = 5
+
+    #     # spawning the client process
+    #     for _ <- 1..max_req do
+    #         # send the request
+    #         {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+
+    #         # send the response back to the parent
+    #         spawn(fn -> send(parent, {:ok, response}) end)
+    #     end
+
+    #     # await all messages from spawned process
+    #     for _ <- 1..max_req do
+    #         receive do
+    #             {:ok, response} -> 
+    #                 assert response.status_code == 200
+    #                 assert response.body == "Bears, Lions, Tigers"
+    #         end
+    #     end
+    # end
+
+    test "accepts multiple requests concurrently on a socket and sends back a response" do
         spawn(HttpServer, :start, [4000])
 
-        parent = self()
+        # url = "http://localhost:4000/wildthings"
 
-        max_req = 5
+        urls = [
+            "http://localhost:4000/wildthings",
+            "http://localhost:4000/bears",
+            "http://localhost:4000/bears/1",
+            "http://localhost:4000/wildlife",
+            "http://localhost:4000/api/bears"
+          ]
 
-        # spawning the client process
-        for _ <- 1..max_req do
-            # send the request
-            {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+        # 1..5
+        urls
+        |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
+        |> Enum.map(&Task.await/1)
+        |> Enum.map(&asser_successful_response/1)
+    end
 
-            # send the response back to the parent
-            spawn(fn -> send(parent, {:ok, response}) end)
-        end
-
-        # await all messages from spawned process
-        for _ <- 1..max_req do
-            receive do
-                {:ok, response} -> 
-                    assert response.status_code == 200
-                    assert response.body == "Bears, Lions, Tigers"
-            end
-        end
+    defp asser_successful_response({:ok, response}) do
+        assert response.status_code == 200
+        # assert response.body == "Bears, Lions, Tigers"
     end
     
-    defp remove_whitespace(text) do
-        String.replace(text, ~r{\s}, "")
-    end
 end
